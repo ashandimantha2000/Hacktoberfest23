98bb6405be3c0d42ea2e98c02c9ab65370a5a02a
ravindu0823 <guestpc87@gmail.com>
2023-10-15 11:50:06 +0530

    chore: plot data values along with the indexes

--- normalize_data.m
+++ normalize_data.m
@@ -1,3 +1,12 @@
 % Generate a random data set of 200 values ranging from 1 to 100
 data_set = rand(1, 200) * 199 + 1;
 % disp(data_set);
+
+% Plot the data
+figure;
+% 'o-' specifies that you want markers and lines
+plot(data_set, 'o-');
+title('Random Dataset');
+xlabel('Index');
+ylabel('Value');
+grid on; % Display grid lines
